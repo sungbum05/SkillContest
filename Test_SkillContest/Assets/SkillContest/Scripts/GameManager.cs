57f841f6b7677b4e8e726bf1af6bd638cf5e9a3e
sungbum05 <sblim05@naver.com>
2022-03-26 19:27:45 +0900

    Hp,Pain_Slider

--- Test_SkillContest/Assets/SkillContest/Scripts/GameManager.cs
+++ Test_SkillContest/Assets/SkillContest/Scripts/GameManager.cs
@@ -6,25 +6,114 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] int Stage = 1;
+    #region ÀÎ½ºÅÏ½º »ý¼º
+    private static GameManager instance;
 
-    [SerializeField] EnemySpanwer enemySpanwer;
+    public static GameManager Instance { 
+        get
+        {
+            if(!instance)
+            {
+                instance = FindObjectOfType(typeof(GameManager)) as GameManager;
+            }
+
+            return instance;
+        } 
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        else if(instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+    #endregion
+
+    public int Stage = 1;
+    public int Pain = 0;
+
+    [Header("¾À °´Ã¼")]
+    public EnemySpanwer enemySpanwer;
+    public PlayerController Player;
+
+    [Header("UI °´Ã¼")]
+    public Slider HpSlider;
+    public Slider PainSlider;
+    public Text HpText;
+    public Text PainText;
 
     // Start is called before the first frame update
     void Start()
     {
+        BasicSetting();
+
         SendEnemyData();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CurrentState();
     }
 
     void SendEnemyData()
     {
         enemySpanwer.ReadEnemyData(
             Resources.Load<TextAsset>($"EnemyPatton_6").text);
+    }
+    
+    void BasicSetting()
+    {
+        switch(Stage)
+        {
+            case 1:
+                Pain = 10;
+                break;
+
+            case 2:
+                Pain = 30;
+                break;
+        }
+
+        enemySpanwer = FindObjectOfType(typeof(EnemySpanwer)) as EnemySpanwer;
+        Player = FindObjectOfType(typeof(PlayerController)) as PlayerController;
+
+        HpSlider = GameObject.Find("HpSlider").gameObject.GetComponent<Slider>();
+        PainSlider = GameObject.Find("PainSlider").gameObject.GetComponent<Slider>();
+        HpText = GameObject.Find("HpTxt").gameObject.GetComponent<Text>();
+        PainText = GameObject.Find("PainTxt").gameObject.GetComponent<Text>();
+
+        HpSlider.value = Player.HP;
+        PainSlider.value = Pain;
+    }
+
+    void CurrentState()
+    {
+        HpText.text = $"HP : {Player.HP}";
+        PainText.text = $"PAIN : {Pain}";
+
+        HpSlider.value = GotoValue(HpSlider.value, Player.HP);
+        PainSlider.value = GotoValue(PainSlider.value, Pain);
+    }
+
+    float GotoValue(float Value, float TargetValue)
+    {
+        if(Value > TargetValue)
+        {
+            Value -= 20 * Time.deltaTime;
+        }
+
+        else if(Value < TargetValue)
+        {
+            Value += 20 * Time.deltaTime;
+        }
+
+        return Value;
     }
 }
